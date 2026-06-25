73c109c5a306a49bed5e7311b4aa217ece14ec7b
elisbanpaco <paccoacchaelias@gmail.com>
2026-06-25 14:18:23 -0500

    Add: congiurations lucid-icons tailwindv4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import db from '@astrojs/db';
 import react from '@astrojs/react';
+import tailwind from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +10,8 @@ export default defineConfig({
     db(),
     react()
   ],
+  vite: {
+    plugins: [tailwind()]
+  }
   // adapter: node() or cloudflare() based on deployment target
 });
