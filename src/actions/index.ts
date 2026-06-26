9a98851d9eb4c36ade4734026c2717801bd505c5
elisbanpaco <paccoacchaelias@gmail.com>
2026-06-26 18:48:46 -0500

    update: index actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,7 +1,9 @@
 import { authActions } from './auth';
 import { postActions } from './posts';
+import { profileActions } from './profile';
 
 export const server = {
   ...authActions,
   ...postActions,
+  ...profileActions,
 };
