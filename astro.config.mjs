063b3faac2591995b3905586ac64e66ecbc7ebaa
elisbanpaco <paccoacchaelias@gmail.com>
2026-06-25 23:52:27 -0500

    Agregar archivos de configuración y dependencias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import db from '@astrojs/db';
 import react from '@astrojs/react';
 import tailwind from '@tailwindcss/vite';
 
@@ -7,7 +6,6 @@ import tailwind from '@tailwindcss/vite';
 export default defineConfig({
   output: 'server',
   integrations: [
-    db(),
     react()
   ],
   vite: {
