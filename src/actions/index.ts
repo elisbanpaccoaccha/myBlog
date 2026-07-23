2c50c91c7329216f3d7d613ef27ebdd8c00a8650
elisbanpaco <paccoacchaelias@gmail.com>
2026-07-22 19:09:58 -0500

    refactor: export interactions actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,10 +2,12 @@ import { authActions } from './auth';
 import { postActions } from './posts';
 import { profileActions } from './profile';
 import { toolsActions } from './tools';
+import { interactionActions } from './interactions';
 
 export const server = {
   ...authActions,
   ...postActions,
   ...profileActions,
   ...toolsActions,
+  ...interactionActions,
 };
